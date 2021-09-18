c757637926a95f19d1b3f5fb8b4e3c04b9af702a
Roney11 <alexsilva270995@gmail.com>
2021-09-18 15:10:40 -0300

    adicionado a opção de prioridade das tarefas e alguns styles

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function openform() {
 
     document.getElementById('cancelar').addEventListener('click', () => {
         document.getElementById('form').style.marginLeft = '-100%'
+        document.getElementById('error').style.display = 'none'
     })
 }
 openform()
@@ -15,45 +16,55 @@ openform()
 function form() {
     
     document.getElementById('error').style.display = 'none'
-    document.getElementById('add-task-btn').addEventListener('click', add)
-
-    function add() {
-        verificarCampo()
-    }
+    document.getElementById('add-task-btn').addEventListener('click', verificarCampo)
 
     function verificarCampo() {
-        const input = document.getElementById('input-task').value
+        const input = document.getElementById('area').value
 
         if(input == false) {
             document.getElementById('error').style.display = 'block'
         }else {
-            document.getElementById('error').style.display = 'none'
+            document.getElementById('no-nota').style.display = 'none'
             creatNotas()
         }
     }
 
     function creatNotas() {
-        const input = document.getElementById('input-task').value
+        const input = document.getElementById('area').value
 
         //Criando um novo elemento
         const newUl = document.createElement('ul')
+        const contentUlBtn = document.createElement('button')
         const contentUl = document.createTextNode(input)
+        newUl.appendChild(contentUlBtn)
         newUl.appendChild(contentUl)
 
         //adicionando o elemento criado ao DOM
         const ulAtual = document.getElementById('tasker-body')
         document.body.insertBefore(newUl, ulAtual)
 
-        document.getElementById('input-task').value = ''
+        document.getElementById('area').value = ''
         document.getElementById('form').style.marginLeft = '-100%'
+
+        //verificando a urgência da tarefa
+        const select = document.getElementById('prioridade')
+        const prioridade = select.options[select.selectedIndex].value
+        console.log(prioridade)
+
+        if(prioridade == 'normal') {
+            contentUlBtn.style.background = 'rgb(4, 175, 4)'
+            newUl.style.borderColor = 'rgb(4, 175, 4)'
+        }
+        if(prioridade == 'alerta') {
+            newUl.style.borderColor = 'rgb(207, 207, 11)'
+            contentUlBtn.style.background = 'rgb(207, 207, 11)'
+        }
+        if(prioridade == 'urgente') {
+            contentUlBtn.style.background = 'rgb(255, 0, 0)'
+            newUl.style.borderColor = 'rgb(255, 0, 0)'
+
+        }
     }
+
 }
 form()
-
-
-
-
-
-
-
-    
