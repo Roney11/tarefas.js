af369d83fa6bf77c19818d5f8dc9f9432e286547
Roney11 <alexsilva270995@gmail.com>
2021-09-19 23:08:58 -0300

    adicionado a data e hora que foi criado a tarefa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 //função para abrir e fechar o formulário
 function openform() {
     document.getElementById('add').addEventListener('click', () => {
@@ -11,7 +12,7 @@ function openform() {
 }
 openform()
 
-//função para fazer a verificação no form e 
+//função para fazer a verificação no form
 //adicionar o conteúdo do form no DOM
 function form() {
     
@@ -20,24 +21,55 @@ function form() {
 
     function verificarCampo() {
         const input = document.getElementById('area').value
+        const select = document.getElementById('prioridade')
+        const prioridade = select.options[select.selectedIndex].value
 
         if(input == false) {
+            document.getElementById('error').style.display = 'block'
+        }if(prioridade == 'desable') {
             document.getElementById('error').style.display = 'block'
         }else {
             document.getElementById('no-nota').style.display = 'none'
             creatNotas()
         }
     }
-
+    //adicionar o conteúdo do form no DOM
     function creatNotas() {
+
         const input = document.getElementById('area').value
 
         //Criando um novo elemento
         const newUl = document.createElement('ul')
+        const h4 = document.createElement('h4')
         const contentUlBtn = document.createElement('button')
         const contentUl = document.createTextNode(input)
         newUl.appendChild(contentUlBtn)
-        newUl.appendChild(contentUl)
+        newUl.appendChild(h4)
+        h4.appendChild(contentUl)
+        
+
+        //Pegando o dia e hora que o arquivo foi criado
+
+        const dia = new Date()
+        const diaAtual = dia.getUTCDate() -1
+        const mesAtual = dia.getUTCMonth() +1
+        const anoAtual = dia.getUTCFullYear()
+        const hora = dia.getHours()
+        const minutos = dia.getMinutes()
+
+        //formatando a data e hora
+        formatoData = (diaAtual < 10 ? `0${diaAtual}`: diaAtual) + (mesAtual < 10 ? `-0${mesAtual}-`: `-${mesAtual}-`) + (anoAtual)
+
+        formatoHoras = (hora < 10 ? `0${hora}:` : `${hora}:`) + (minutos < 10 ? `0${minutos}` : minutos)
+
+        const p = document.createElement('p')
+        const contentUlP = document.createTextNode(`Criado dia ${formatoData} às ${formatoHoras}`)
+        
+        newUl.appendChild(p)
+        p.appendChild(contentUlP)
+
+
+
 
         //adicionando o elemento criado ao DOM
         const ulAtual = document.getElementById('tasker-body')
@@ -49,7 +81,6 @@ function form() {
         //verificando a urgência da tarefa
         const select = document.getElementById('prioridade')
         const prioridade = select.options[select.selectedIndex].value
-        console.log(prioridade)
 
         if(prioridade == 'normal') {
             contentUlBtn.style.background = 'rgb(4, 175, 4)'
@@ -62,9 +93,8 @@ function form() {
         if(prioridade == 'urgente') {
             contentUlBtn.style.background = 'rgb(255, 0, 0)'
             newUl.style.borderColor = 'rgb(255, 0, 0)'
-
         }
     }
-
 }
 form()
+
